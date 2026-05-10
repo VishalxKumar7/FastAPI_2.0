bfabf4afa365b7406f08e8131c4b38b63bd9cede
vishalxkumar7 <vishalxkumar7@gamil.com>
2026-05-10 12:47:28 +0530

    fixed

--- TODO/database.py
+++ TODO/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg2://postgres:vishal123@localhost:5432/TodoApplicationDatabase'
+import os
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
